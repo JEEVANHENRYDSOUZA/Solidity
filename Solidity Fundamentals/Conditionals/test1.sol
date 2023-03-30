a1694a1042b3c8b5b787de5689469444d8decbf4
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2023-03-29 21:46:23 -0400

    Create test1.sol

---
+++ Solidity Fundamentals/Conditionals/test1.sol
@@ -0,0 +1,18 @@
+pragma solidity >=0.4.22 <=0.8.17;
+
+contract Comparison {
+    function compare(int256 a, int256 b) public pure returns (int256) {
+        // Write your code here
+        if(a == b){
+            return 0;
+        }
+        else if(a>b){
+            return 1;
+        }
+        else if(a<b){
+            return -1;
+        }
+    
+
+    }
+}
