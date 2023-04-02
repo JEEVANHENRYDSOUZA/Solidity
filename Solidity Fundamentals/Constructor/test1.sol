6684da504bb1ea1b929996ddddc1216f819a3e44
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2023-04-02 13:21:22 -0400

    Create test1.sol

---
+++ Solidity Fundamentals/Constructor/test1.sol
@@ -0,0 +1,11 @@
+// called only once when we deploy the smart contract
+// example : 
+contract jeevan{
+      constructor () {
+      
+          // used to find the owner of the account
+      }
+
+
+
+}
