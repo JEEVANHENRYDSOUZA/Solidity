b3b0fb377a3ea6e5d7f6812269c8c1ee4af810f4
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2023-04-02 13:04:57 -0400

    Create test2.sol

---
+++ Solidity Fundamentals/Receiving & Sending Eth in Smart Contract/test2.sol
@@ -0,0 +1,36 @@
+pragma solidity >=0.4.22 <=0.8.17;
+
+contract Balances {
+    // Write your code here
+            mapping(address=>uint256) balances;
+
+
+
+
+            fallback() external payable{
+
+
+
+                        balances[msg.sender]+=msg.value;
+            }
+
+
+        receive () external payable{
+                balances[msg.sender]+=msg.value;
+
+
+
+        }
+
+
+
+
+            function getAmountSent(address addr) public view returns(uint256){
+                            return balances[addr];
+
+
+            }
+
+
+
+}
